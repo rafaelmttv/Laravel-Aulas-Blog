dbea643a63cca6c7c62673b38b67cc69168317ca
Rafael Tavares <rafaelmttv@gmail.com>
2022-08-14 12:20:33 -0300

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index');
+    }
+
+    public function index()
+    {
+        # code...
+        return view('home');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+}
---
+++ resources/views/projects.blade.php
@@ -0,0 +1,4 @@
+
+<h1>Home</h1>
+<a href="{{ route('login')}} ">Login</a>
+
