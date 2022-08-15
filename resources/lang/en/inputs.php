ee06688898594f330eaa36e38e06bdfd0f8fab97
Rafael Tavares <rafaelmttv@gmail.com>
2022-08-15 13:16:34 -0300

    Implementando translations

---
+++ resources/lang/en/inputs.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'label' => [
+        'first_name' => 'First name',
+        'last_name' => 'Last name',
+        'email_address' => 'E-mail address',
+        'password' => 'Password',
+        'confirm_password' => 'Confirm password'
+        ]
+];
+
+?>
